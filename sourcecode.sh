6166b66c834fe3d51a40eca059c465118f81fdd0
arun777akj <arun777akj@gmail.com>
2025-02-04 14:44:38 -0500

    Create a source code which calls method and display value of m

---
+++ sourcecode.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# calculate_m.sh
+
+# Input values
+y=10
+x=2
+b=1
+
+# Calculate the value of m
+
+RESULT=$(./function_to_find_m.sh 10 2 1)
+
+# Output the result
+echo "The value of m is: $RESULT"
